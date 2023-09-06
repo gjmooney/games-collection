d3c7f0fb55b9b8c8e4124db8b7c5cf60a5d5951b
Greg <gjmooney@asu.edu>
2023-09-06 14:20:34 +0200

    Change db load

--- lib/db.ts
+++ lib/db.ts
@@ -3,16 +3,7 @@ import { drizzle } from "drizzle-orm/neon-http";
 
 neonConfig.fetchConnectionCache = true;
 
-let isConnected = false;
-export const connectToDb = async () => {
-  if (isConnected) {
-    console.log("Alredy connected to DB");
-  }
+const sql = neon(process.env.DRIZZLE_DATABASE_URL!);
+const db = drizzle(sql);
 
-  try {
-    const sql = neon(process.env.DRIZZLE_DATABASE_URL!);
-    const db = drizzle(sql);
-  } catch (error) {
-    console.log("Error connecting to DB", error);
-  }
-};
+export default db;
