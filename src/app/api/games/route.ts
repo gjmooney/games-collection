1deabdba128464301987cbbc8e8cce9e6b70fc2b
Greg <gjmooney@asu.edu>
2023-09-19 12:48:54 +0200

    Add cursor to query

--- src/app/api/games/route.ts
+++ src/app/api/games/route.ts
@@ -1,8 +1,10 @@
 import db from "@/db/db";
-import { games, users, usersToGames } from "@/db/schema";
+import { GameInfoSelect, games, users, usersToGames } from "@/db/schema";
 import { auth } from "@clerk/nextjs";
-import { eq } from "drizzle-orm";
+import { and, asc, eq, gt } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
+
+const LIMIT = 10;
 
 export async function GET(req: NextRequest) {
   try {
@@ -12,24 +14,62 @@ export async function GET(req: NextRequest) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
+    const { searchParams } = new URL(req.url);
+    console.log("searchParams", searchParams);
+    const cursor = searchParams.get("cursor");
+
     const user = await db
       .select({ id: users.id })
       .from(users)
       .where(eq(users.clerkId, clerkId));
 
-    const gamesFromDb = await db
-      .select({
-        id: games.id,
-        gameName: games.gameName,
-        platform: games.platform,
-        imgUrl: games.imgUrl,
-        store: games.store,
-      })
-      .from(usersToGames)
-      .innerJoin(games, eq(usersToGames.gameId, games.id))
-      .where(eq(usersToGames.userId, user[0].id));
+    let gamesFromDb: Omit<GameInfoSelect, "createdAt">[] = [];
 
-    return NextResponse.json(gamesFromDb);
+    if (cursor) {
+      gamesFromDb = await db
+        .select({
+          id: games.id,
+          gameName: games.gameName,
+          platform: games.platform,
+          imgUrl: games.imgUrl,
+          store: games.store,
+        })
+        .from(usersToGames)
+        .innerJoin(games, eq(usersToGames.gameId, games.id))
+        .orderBy(asc(games.id))
+        .where(
+          and(
+            eq(usersToGames.userId, user[0].id),
+            gt(games.id, parseInt(cursor)) // gt is like a skip 1
+          )
+        )
+
+        .limit(LIMIT);
+    } else {
+      gamesFromDb = await db
+        .select({
+          id: games.id,
+          gameName: games.gameName,
+          platform: games.platform,
+          imgUrl: games.imgUrl,
+          store: games.store,
+        })
+        .from(usersToGames)
+        .innerJoin(games, eq(usersToGames.gameId, games.id))
+        .where(eq(usersToGames.userId, user[0].id))
+        .orderBy(asc(games.id))
+        .limit(LIMIT);
+    }
+
+    let nextCursor = null;
+
+    // aka there is a next page
+    if (gamesFromDb.length === LIMIT) {
+      nextCursor = gamesFromDb[LIMIT - 1].id;
+    }
+    console.log("nextCursor", nextCursor);
+
+    return NextResponse.json({ gamesFromDb, nextCursor });
   } catch (error) {
     console.log("error", error);
   }
