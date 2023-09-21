f9328658b7169e46e97ed719ae8d6e7824d0d792
Greg <gjmooney@asu.edu>
2023-09-21 18:24:44 +0200

    Add fancy title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-      Stop buying games more than once! <br />
-      Make a list! Check the list! <br />
-      The list is your new god!
+    <main className="flex flex-col items-center justify-between p-24 gap-16">
+      <h1 className="text-8xl h-28 bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent">
+        BuyWise
+      </h1>
+      <p>
+        Stop buying games more than once! <br />
+        Make a list! Check the list! <br />
+        The list is your new god!
+      </p>
     </main>
   );
 }
