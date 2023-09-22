b7deed76a0acfc95e0ab1db563c916566b2a0c32
Greg <gjmooney@asu.edu>
2023-09-22 13:19:36 +0200

    Change key

--- src/app/(routes)/update/page.tsx
+++ src/app/(routes)/update/page.tsx
@@ -5,33 +5,39 @@ interface pageProps {}
 const page = async ({}: pageProps) => {
   const storeFronts = [
     {
+      id: 1,
       storeName: "steam",
       imageUrl: "",
       apiUrlEndpoint: "steam",
     },
     {
+      id: 2,
       storeName: "humble",
       imageUrl: "",
       apiUrlEndpoint: "humble",
     },
     {
+      id: 3,
       storeName: "nintendo",
       imageUrl: "",
       apiUrlEndpoint: "nintendo",
     },
     {
+      id: 4,
       storeName: "playstation",
       region: "US",
       imageUrl: "",
       apiUrlEndpoint: "playstation-us",
     },
     {
+      id: 5,
       storeName: "playstation",
       region: "EU",
       imageUrl: "",
       apiUrlEndpoint: "playstation-eu",
     },
     {
+      id: 6,
       storeName: "itch.io",
       imageUrl: "",
       apiUrlEndpoint: "itch",
@@ -42,7 +48,7 @@ const page = async ({}: pageProps) => {
     <main className="flex gap-9 items-center flex-wrap justify-around">
       {storeFronts.map((store) => (
         <StoreCard
-          key={store.storeName}
+          key={store.id}
           storeName={store.storeName}
           imageUrl={store.imageUrl}
           region={store.region}
