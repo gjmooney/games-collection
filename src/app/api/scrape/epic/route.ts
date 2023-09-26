97365e977883095c43777f1b9536b05261cc8649
Greg <gjmooney@asu.edu>
2023-09-26 13:49:17 +0200

    Add epic route

--- src/app/api/scrape/epic/route.ts
+++ src/app/api/scrape/epic/route.ts
@@ -1,15 +1,72 @@
+import { getDecryptedCookie } from "@/db/db";
+import { auth } from "@clerk/nextjs";
 import { NextRequest, NextResponse } from "next/server";
-import { ZodError } from "zod";
+
+type EpicStoreScrape = {
+  items: {
+    description: string;
+  }[];
+};
 
 export async function GET(req: NextRequest) {
   try {
-    return NextResponse.json("epic", { status: 200 });
-  } catch (error: any) {
-    // Type parsing error
-    if (error instanceof ZodError) {
-      return new Response("[PARSE_ERROR]" + error.message, { status: 422 });
+    const { userId } = auth();
+
+    if (!userId) {
+      return new Response("Unauthorized", { status: 401 });
     }
-    console.log("ERROR", error.message);
+
+    const decodedSsoCookie = await getDecryptedCookie(userId, "epic_sso");
+    const decodedBearerCookie = await getDecryptedCookie(userId, "epic_bearer");
+
+    const cookie = `EPIC_SSO=${decodedSsoCookie}; EPIC_BEARER_TOKEN=${decodedBearerCookie}`;
+    const firstUrl =
+      "https://www.epicgames.com/account/v2/payment/ajaxGetOrderHistory?sortDir=DESC&sortBy=DATE&locale=en-US";
+
+    let response = await fetch(firstUrl, {
+      headers: {
+        Cookie: cookie,
+      },
+    });
+
+    let data = await response.json();
+    let nextPageToken = data.nextPageToken;
+
+    const epicGames = data.orders.map((game: EpicStoreScrape) => ({
+      gameName: game.items[0].description,
+      store: "Epic",
+      platform: "PC",
+      imgUrl: "",
+    }));
+
+    while (nextPageToken) {
+      const nextUrl = `https://www.epicgames.com/account/v2/payment/ajaxGetOrderHistory?sortDir=DESC&sortBy=DATE&nextPageToken=${nextPageToken}&locale=en-US`;
+
+      response = await fetch(nextUrl, {
+        headers: {
+          Cookie: cookie,
+        },
+      });
+
+      data = await response.json();
+      nextPageToken = data.nextPageToken;
+
+      const nextGamePage = data.orders.map((game: EpicStoreScrape) => ({
+        gameName: game.items[0].description,
+        store: "Epic",
+        platform: "PC",
+        imgUrl: "",
+      }));
+
+      epicGames.push(...nextGamePage);
+    }
+
+    return NextResponse.json(epicGames, { status: 200 });
+  } catch (error: any) {
+    if (error instanceof SyntaxError) {
+      return new Response("Access Forbidden", { status: 403 });
+    }
+    console.error("ERROR", error);
     return new Response("Something went wrong", { status: 500 });
   }
 }
